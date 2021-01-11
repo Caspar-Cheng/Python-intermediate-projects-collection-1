3ec402f959115590e126bd7c89f121165ab0b64f
Caspar <52498280+Caspar-Cheng@users.noreply.github.com>
2021-01-11 12:33:46 +1000

    Update snake.py

--- snake game/snake.py
+++ snake game/snake.py
@@ -1,48 +1,56 @@
-# create snake class to cooridinate snake and make it moving
+# create snake class to initialize object
+
 from turtle import Turtle
 
-START_POSITION = [(0, 0 ), (-20, 0), (-40, 0)]
+START_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_STEPS = 20
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+
 class Snake:
-    
+
     def __init__(self):
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
-    
+
     def create_snake(self):
         for position in START_POSITION:
-            new_seg = Turtle("square")
-            new_seg.color("white")
-            new_seg.penup()
-            new_seg.goto(position)
-            self.segments.append(new_seg)
-    
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_seg = Turtle("square")
+        new_seg.color("white")
+        new_seg.penup()
+        new_seg.goto(position)
+        self.segments.append(new_seg)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
     def move(self):
-        """connected all the segments of the snake to move to the previous segment's position"""
-        for seg_num in range(len(self.segments)-1, 0, -1):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        
+
+            # make the first segment move to make the snake moving
         self.head.forward(MOVE_STEPS)
 
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
-    
+
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
     def left(self):
         if self.head.heading() != RIGHT:
-            self.head.setheading(LEFT)   
+            self.head.setheading(LEFT)
 
     def right(self):
         if self.head.heading() != LEFT:

463621d0bbeed0fad5c4d4c352c2b4d965beeb07
Caspar <52498280+Caspar-Cheng@users.noreply.github.com>
2021-01-16 15:42:49 +1000

    Update main.py

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -1,5 +1,7 @@
 import pandas
 
+# dictionary needs to be pandas.DataFrame(dict) before using iterrows(); 
+# here is already a DataFrame format after the pandas.read_csv() method
 alphabet_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 letter_dict = {row.letter : row.code for (index, row) in alphabet_data.iterrows()}
